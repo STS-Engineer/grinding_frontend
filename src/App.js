b3f5bdeda5d3f24a8d6e7fbd48d4125e20fbb80a
STS-Engineer <administration.sts@avocarbon.com>
2025-01-17 16:52:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import Listregleur from './pages/List de regleurs';
 import Listoperateur from './pages/Listoperateurs';
 import Ajouterposte from './pages/Ajouter poste de controle';
 import Ajouteroutil from './pages/Ajouter outil';
+import ActionHistory from './pages/ActionHistory ';
 
 
 function App() {
@@ -40,6 +41,7 @@ function App() {
       <Route path="/listregleur" element={<Listregleur/>} />
       <Route path="/Ajouterproblemepostedecontrole" element={<Ajouterposte/>} />
       <Route path="/Ajouteroutil" element={<Ajouteroutil/>} />
+     <Route path="/history" element={<ActionHistory/>} />
       </Routes>
      </Router>
     </div>

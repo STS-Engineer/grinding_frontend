915a9d9c7b5c5009d8ae22007d4c6b196242c69b
STS-Engineer <administration.sts@avocarbon.com>
2025-01-23 09:46:31 +0100

    Update Ajouterinspectiondefaut.jsx

--- src/pages/Ajouterinspectiondefaut.jsx
+++ src/pages/Ajouterinspectiondefaut.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Ajouterregleur.css';
 import { Modal, Input, message, Select,  Form, Button } from 'antd'; // Import Ant Design components
 import { useNavigate } from 'react-router-dom';
 import ActionHistory from './ActionHistory ';
